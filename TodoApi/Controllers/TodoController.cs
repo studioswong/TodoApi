75ba95ab3d062d82a1cec44336b354541ea1bcf4
COTS\susanna.wong <susanna.wong@capitalontap.com>
2018-12-05 09:34:40 +0000

    adding the file folder

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
